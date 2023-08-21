e798b2c8fc407f5bfde0bed4ff7cfc36899d8671
Luis Duque <luisfpetrucci@gmail.com>
2023-08-21 12:09:10 -0500

    fix(nvim): updated neorg notes location

--- dot_config/nvim/lua/plugins/languages/neorg/neorg.lua
+++ dot_config/nvim/lua/plugins/languages/neorg/neorg.lua
@@ -12,7 +12,7 @@ return {
 				["core.dirman"] = { -- Manages Neorg workspaces
 					config = {
 						workspaces = {
-							notes = "~/.notes",
+							notes = "~/Documents/notes",
 						},
 						default_workspace = "notes",
 					},
