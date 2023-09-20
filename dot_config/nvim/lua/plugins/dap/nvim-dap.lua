fa31469f313f2c9629ac64c9d86bec98bb7130dc
Luis Duque <luisfpetrucci@gmail.com>
2023-09-20 18:16:19 -0500

    feat(nvim): updated dap keymaps

--- dot_config/nvim/lua/plugins/dap/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/dap/nvim-dap.lua
@@ -6,16 +6,17 @@ return {
 		dap.set_log_level("TRACE")
 	end,
 	keys = {
-		{ "sC", "<CMD>lua require('dap').run_to_cursor()<CR>", desc = "run_to_cursor" },
-		{ "sb", "<CMD>lua require('dap').toggle_breakpoint()<CR>", desc = "breakpoint" },
-		{ "sc", "<CMD>lua require('dap').continue()<CR>", desc = "continue" },
-		{ "si", "<CMD>lua require('dap').step_into()<CR>", desc = "into" },
-		{ "sl", "<CMD>lua require('dap').run_last()<CR>", desc = "run_last" },
-		{ "sn", "<CMD>lua require('dap').step_over()<CR>", desc = "step_over" },
-		{ "sr", "<CMD>lua require('dap').restart()<CR>", desc = "restart" },
-		{ "st", "<CMD>lua require('dap').terminate()<CR>", desc = "terminate" },
+		{ "<F5>", "<CMD>lua require('dap').continue()<CR>", desc = "continue" },
+		{ "<F9>", "<CMD>lua require('dap').toggle_breakpoint()<CR>", desc = "breakpoint" },
+		{ "<F10>", "<CMD>lua require('dap').step_over()<CR>", desc = "step_over" },
+		{ "<F11>", "<CMD>lua require('dap').step_into()<CR>", desc = "into" },
+		{ "<S-F5>", "<CMD>lua require('dap').terminate()<CR>", desc = "terminate" },
+
+		{ "<leader>dc", "<CMD>lua require('dap').run_to_cursor()<CR>", desc = "run_to_cursor" },
+		{ "<leader>dl", "<CMD>lua require('dap').run_last()<CR>", desc = "run_last" },
+		{ "<leader>dr", "<CMD>lua require('dap').restart()<CR>", desc = "restart" },
 		{
-			"ss",
+			"<leader>ds",
 			"<CMD>lua require('dap.ui.widgets').centered_float(require('dap.ui.widgets').scopes)<CR>",
 			desc = "scopes",
 		},
