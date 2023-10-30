5f6ea80e8811e92fdfe6a28711227f6b9120c134
Luis Duque <luisfpetrucci@gmail.com>
2023-10-30 18:06:25 -0500

    fix(luasnip): updated confirm key

--- dot_config/nvim/lua/plugins/lsp/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -38,7 +38,7 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-h>"] = cmp.mapping.confirm({
+				["<CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Replace,
 					select = true,
 				}),
