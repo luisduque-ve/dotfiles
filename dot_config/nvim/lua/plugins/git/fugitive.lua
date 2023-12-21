4ae0a8cc17cf5852299be2741935c3348f84e9db
Luis Duque <luisfpetrucci@gmail.com>
2023-12-21 16:52:33 -0500

    fix(nvim): updated fugitive mappings

--- dot_config/nvim/lua/plugins/git/fugitive.lua
+++ dot_config/nvim/lua/plugins/git/fugitive.lua
@@ -2,13 +2,13 @@ return {
 	"tpope/vim-fugitive",
 	cmd = { "Git" },
 	keys = {
+		{ "<leader>gX", "<CMD>Gread<CR>", desc = "discard_changes" },
 		{ "<leader>ga", "<CMD>Git add % <CR>", desc = "add" },
-		{ "<leader>ge", ":Git ", desc = "command" },
 		{ "<leader>gg", "<CMD>0Git<CR>", desc = "fugitive" },
 		{ "<leader>gl", "<CMD>0Git log --oneline<CR>", desc = "log" },
 		{ "<leader>gm", ":GMove ", desc = "move" },
 		{ "<leader>gp", "<CMD>Git push<CR>", desc = "push" },
 		{ "<leader>gr", ":GRename ", desc = "rename" },
-		{ "<leader>gx", "<CMD>Gread<CR>", desc = "discard_changes" },
+		{ "<leader>gx", ":Git ", desc = "execute" },
 	},
 }
