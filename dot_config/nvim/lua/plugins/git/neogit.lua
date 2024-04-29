381cc7d431031947521f85d6246161cd02af2997
Luis Duque <luisfpetrucci@gmail.com>
2024-04-28 23:02:38 -0500

    fix(nvim): updated neogit kind

--- dot_config/nvim/lua/plugins/git/neogit.lua
+++ dot_config/nvim/lua/plugins/git/neogit.lua
@@ -7,6 +7,6 @@ return {
 	},
 	config = true,
 	keys = {
-		{ "<leader>gg", "<CMD>Neogit<CR>", desc = "neogit" },
+		{ "<leader>gg", ":lua require('neogit').open({ kind = 'replace'})<cr>", desc = "neogit" },
 	},
 }
