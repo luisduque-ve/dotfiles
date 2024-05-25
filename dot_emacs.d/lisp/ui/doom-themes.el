2fd9364e95a758727bbe47de4ac5ce3cd74fd7e6
Luis Duque <luisfpetrucci@gmail.com>
2024-05-25 18:27:53 -0500

    fix(emacs): remove error blink on doom-themes

--- dot_emacs.d/lisp/ui/doom-themes.el
+++ dot_emacs.d/lisp/ui/doom-themes.el
@@ -1,5 +1,4 @@
 (use-package doom-themes
-  :ensure t
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
@@ -7,7 +6,7 @@
   (load-theme 'doom-gruvbox-light t)
 
   ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
+  ;; (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
