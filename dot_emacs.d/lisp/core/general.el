aeff3f2216263bafd9fdb635e818652769e613bb
Luis Duque <luisfpetrucci@gmail.com>
2024-05-26 00:47:02 -0500

    feat(emacs): added general.el

---
+++ dot_emacs.d/lisp/core/general.el
@@ -0,0 +1,19 @@
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-create-definer my-leader-def
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (my-leader-def
+    "SPC" '(consult-find :which-key "find_file")
+    "s" '(:which-key "search") ;; search prefix
+    "sb" '(consult-buffer :which-key "buffers")
+    "sc" '(execute-extended-command :which-key "commands")
+    "\"" '(split-window-below :which-key "split")
+    "%" '(split-window-right :which-key "vsplit")
+    "b" '(:which-key "buffers") ;; buffers prefix
+    "bc" '(evil-delete-buffer :which-key "close_all")
+    "bd" '(kill-buffer :which-key "close")
+    "bo" '(delete-other-windows :which-key "only")))
