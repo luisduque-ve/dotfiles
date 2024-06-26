6ff55c61ae8859292273f913dedb580ebfa05fed
Luis Duque <luisfpetrucci@gmail.com>
2024-06-26 00:21:53 -0500

    fix(nvim): hardtime plugin issue

--- dot_config/nvim/lua/plugins/core/hardtime.lua
+++ dot_config/nvim/lua/plugins/core/hardtime.lua
@@ -1,6 +1,3 @@
--- See this, I do not use lazyvim, the distro, but is giving me
--- issues with lazy only the package manager
--- https://github.com/m4xshen/hardtime.nvim/issues/74#issuecomment-2067659330
 return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
@@ -8,11 +5,5 @@ return {
 		allow_different_keys = true,
 	},
 	command = "Hardtime",
-	event = "BufEnter",
-	keys = {
-		{ "n", "j", "<cmd>Hardtime<CR>", desc = "Hardtime" },
-		{ "n", "k", "<cmd>Hardtime<CR>", desc = "Hardtime" },
-		{ "n", "gj", "<cmd>Hardtime<CR>", desc = "Hardtime" },
-		{ "n", "gk", "<cmd>Hardtime<CR>", desc = "Hardtime" },
-	},
+	event = "VeryLazy",
 }
