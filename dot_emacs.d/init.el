78ce9e1902768b6be5be6c380a4d28e9dfd3db3b
Luis Duque <luisfpetrucci@gmail.com>
2024-08-21 17:03:40 -0500

    feat(emacs): added evil mode

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -1,1 +1,46 @@
--- hello
+(setq custom-file "~/.emacs.d/custom.el")
+
+(unless (file-exists-p custom-file)
+  (with-temp-buffer (write-file custom-file)))
+
+(load custom-file)
+
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-gruvbox-light t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+
+(setq doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 17 :weight 'semi-bold))
+(setq display-line-numbers-type 'relative)
+(setq org-directory "~/org/")
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
