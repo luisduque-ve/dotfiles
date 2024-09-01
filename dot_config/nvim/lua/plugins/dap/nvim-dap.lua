ca62f97071be01efd2e5d0629a4f3a6011e6300a
Luis Duque <luisfpetrucci@gmail.com>
2024-09-01 11:51:58 -0500

    fix(nvim): nvim-dap updated highlight groups

--- dot_config/nvim/lua/plugins/dap/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/dap/nvim-dap.lua
@@ -13,4 +13,11 @@ return {
 		{ "<F10>", "<CMD>lua require('dap').step_over()<CR>", desc = "step_over" },
 		{ "<F11>", "<CMD>lua require('dap').step_into()<CR>", desc = "into" },
 	},
+	config = function()
+		local sign = vim.fn.sign_define
+		sign("DapBreakpoint", { text = "B", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+		sign("DapBreakpointCondition", { text = "C", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+		sign("DapLogPoint", { text = "L", texthl = "DapLogPoint", linehl = "", numhl = "" })
+		sign("DapStopped", { text = "->", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+	end,
 }
