23f6826763c9cc8d1053343f769d015f14e4cfae
Luis Duque <luisfpetrucci@gmail.com>
2024-10-17 16:00:52 -0500

    fix(nvim): set orgmode initial folding style

--- dot_config/nvim/lua/plugins/languages/org/orgmode.lua
+++ dot_config/nvim/lua/plugins/languages/org/orgmode.lua
@@ -7,6 +7,7 @@ return {
 		require("orgmode").setup({
 			org_agenda_files = "~/org/**/*",
 			org_default_notes_file = "~/org/refile.org",
+			org_startup_folded = "showeverything",
 			mappings = {
 				org_return_uses_meta_return = true,
 			},
