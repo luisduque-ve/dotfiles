e675050faacfc0866c861d43006b3567aecc0a93
Luis Duque <luisfpetrucci@gmail.com>
2024-10-23 10:33:57 -0500

    refactor(nix): updated nix file organization

---
+++ nix/common-cli.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+[
+  pkgs.eza # ls replacement
+  pkgs.fish # shell
+  pkgs.fzf # fuzzy finder
+  pkgs.ripgrep # grep replacement
+  pkgs.neovim # text editor
+  pkgs.zellij # terminal multiplexer
+  pkgs.nixfmt-rfc-style # nix formatter
+]
