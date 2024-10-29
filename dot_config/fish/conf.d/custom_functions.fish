314db513017228cb9e89a9bcecef6db6efb32835
Luis Duque <luisfpetrucci@gmail.com>
2024-10-29 10:18:49 -0500

    fix(fish): updated custom_cat to add recursive flag

--- dot_config/fish/conf.d/custom_functions.fish
+++ dot_config/fish/conf.d/custom_functions.fish
@@ -1,12 +1,19 @@
 function custom_cat
-    if test (count $argv) -eq 0
-        echo "Usage: custom_cat <file_extension>"
+    if test (count $argv) -lt 2
+        echo "Usage: custom_cat <file_extension> <recursive_flag>"
         return 1
     end
 
     set ext $argv[1]
+    set recursive $argv[2]
 
-    for file in (find . -name "*.$ext" -type f)
+    if test $recursive = "true"
+        set find_command "find . -name '*.$ext' -type f"
+    else
+        set find_command "find . -maxdepth 1 -name '*.$ext' -type f"
+    end
+
+    for file in (eval $find_command)
         echo "### $file"
         echo ""
         cat $file
