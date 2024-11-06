17cc769c5dc0368d6942d53049dacbd26f484848
Luis Duque <luisfpetrucci@gmail.com>
2024-11-06 11:23:01 -0500

    feat(nvim): deprecated treesitter incremental selection

--- dot_config/nvim/lua/plugins/treesitter/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter/treesitter.lua
@@ -43,15 +43,6 @@ return {
         enable = true, -- false will disable the whole extension
         additional_vim_regex_highlighting = false,
       },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<CR>",
-          scope_incremental = "<CR>",
-          node_incremental = "<TAB>",
-          node_decremental = "<S-TAB>",
-        },
-      },
     })
   end,
   keys = {
