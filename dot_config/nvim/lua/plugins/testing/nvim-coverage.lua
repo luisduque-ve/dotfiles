eaec00c7c7026bcc0f502b065bc296710d65acdf
Luis Duque <luisfpetrucci@gmail.com>
2024-11-06 16:49:45 -0500

    fix(nvim): added test coverage mapping

--- dot_config/nvim/lua/plugins/testing/nvim-coverage.lua
+++ dot_config/nvim/lua/plugins/testing/nvim-coverage.lua
@@ -1,10 +1,13 @@
 return {
-	"andythigpen/nvim-coverage",
-	ft = "python", -- only working with python right now
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		require("coverage").setup({})
-	end,
+  "andythigpen/nvim-coverage",
+  ft = "python", -- only working with python right now
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("coverage").setup({})
+  end,
+  keys = {
+    { "<leader>tc", "<CMD>Coverage<CR><CMD>CoverageSummary<CR>", desc = "coverage" },
+  },
 }
