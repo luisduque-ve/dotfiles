fa36603813f5bc2cdead0fbd00be53e68f6b0d79
Luis Duque <luisfpetrucci@gmail.com>
2024-11-07 10:15:10 -0500

    Revert "fix(nvim): updated which-key layout"
    This reverts commit 90e3956f5dae007055e5fb907628209ccbb94a01.

--- dot_config/nvim/lua/plugins/ui/which-key.lua
+++ dot_config/nvim/lua/plugins/ui/which-key.lua
@@ -1,26 +1,23 @@
 return {
-  "folke/which-key.nvim",
-  dependencies = { "echasnovski/mini.icons", "nvim-tree/nvim-web-devicons" },
-  lazy = false,
-  config = function()
-    local wk = require("which-key")
-    wk.setup({
-      preset = "modern",
-    })
-    wk.add({
-      { "<leader>b",  group = "+buffer" },
-      { "<leader>c",  group = "+ai",                                                         mode = { "n", "v" } },
-      { "<leader>d",  group = "+debug" },
-      { "<leader>g",  group = "+git" },
-      { "<leader>o",  group = "+org" },
-      { "<leader>gd", group = "+diff" },
-      { "<leader>s",  group = "+search" },
-      { "<leader>t",  group = "+test" },
-      { "<leader>u",  group = "+utils" },
-      { "<leader>uc", group = "+change_case_to" },
-      { "<leader>um", group = "+markdown" },
-      { "<leader>uw", group = "+window" },
-      { "<leader>w",  '<cmd>lua require("which-key").show({keys = "<c-w>", loop=true})<cr>', desc = "window" },
-    })
-  end,
+	"folke/which-key.nvim",
+	dependencies = { "echasnovski/mini.icons", "nvim-tree/nvim-web-devicons" },
+	lazy = false,
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>b", group = "+buffer" },
+			{ "<leader>c", group = "+ai", mode = { "n", "v" } },
+			{ "<leader>d", group = "+debug" },
+			{ "<leader>g", group = "+git" },
+			{ "<leader>o", group = "+org" },
+			{ "<leader>gd", group = "+diff" },
+			{ "<leader>s", group = "+search" },
+			{ "<leader>t", group = "+test" },
+			{ "<leader>u", group = "+utils" },
+			{ "<leader>uc", group = "+change_case_to" },
+			{ "<leader>um", group = "+markdown" },
+			{ "<leader>uw", group = "+window" },
+			{ "<leader>w", '<cmd>lua require("which-key").show({keys = "<c-w>", loop=true})<cr>', desc = "window" },
+		})
+	end,
 }
