aeb8c58f43956acef3b856892bb8116a7ad25c24
Luis Duque <luisfpetrucci@gmail.com>
2024-11-14 10:20:37 -0500

    fix(nvim): updated dap mappings

--- dot_config/nvim/lua/plugins/dap/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/dap/nvim-dap.lua
@@ -1,23 +1,36 @@
 return {
-	"mfussenegger/nvim-dap",
-	event = "VeryLazy",
-	init = function()
-		local dap = require("dap")
-		dap.set_log_level("TRACE")
-	end,
-	keys = {
-		-- This follows the vscode style as much as possible, to easy
-		-- work in a vscode environment when needed
-		{ "<F5>", "<CMD>lua require('dap').continue()<CR>", desc = "continue" },
-		{ "<F9>", "<CMD>lua require('dap').toggle_breakpoint()<CR>", desc = "breakpoint" },
-		{ "<F10>", "<CMD>lua require('dap').step_over()<CR><CMD>normal! zz<CR>", desc = "step_over" },
-		{ "<F11>", "<CMD>lua require('dap').step_into()<CR><CMD>normal! zz<CR>", desc = "into" },
-	},
-	config = function()
-		local sign = vim.fn.sign_define
-		sign("DapBreakpoint", { text = "B", texthl = "DapBreakpoint", linehl = "", numhl = "" })
-		sign("DapBreakpointCondition", { text = "C", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
-		sign("DapLogPoint", { text = "L", texthl = "DapLogPoint", linehl = "", numhl = "" })
-		sign("DapStopped", { text = "->", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
-	end,
+  "mfussenegger/nvim-dap",
+  event = "VeryLazy",
+  init = function()
+    local dap = require("dap")
+    dap.set_log_level("TRACE")
+  end,
+  config = function()
+    local sign = vim.fn.sign_define
+    local dap = require('dap')
+
+    sign("DapBreakpoint", { text = "", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+    sign("DapBreakpointCondition", { text = "", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+    sign("DapLogPoint", { text = "", texthl = "DapLogPoint", linehl = "", numhl = "" })
+    sign("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+
+    -- This follows the vscode style as much as possible, to ease
+    -- work in a vscode environment when needed
+    vim.keymap.set("n", "<F5>", dap.continue, { desc = "continue" })
+    vim.keymap.set('n', "<F7>", function()
+      dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+    end, { desc = "log_point" })
+    vim.keymap.set('n', "<F8>", function()
+      dap.set_breakpoint(vim.fn.input('Condition: '), nil, nil)
+    end, { desc = "conditional_point" })
+    vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "breakpoint" })
+    vim.keymap.set("n", "<F10>", function()
+      dap.step_over()
+      vim.cmd("normal! zz")
+    end, { desc = "step_over" })
+    vim.keymap.set("n", "<F11>", function()
+      dap.step_into()
+      vim.cmd("normal! zz")
+    end, { desc = "into" })
+  end,
 }
