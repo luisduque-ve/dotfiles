b8839a207cb20c0b73fefa6665a79accd16508ff
Luis Duque <luisfpetrucci@gmail.com>
2025-01-01 16:24:41 -0500

    fix(nvim): updated which-key labels

--- dot_config/nvim/lua/plugins/ui/which-key.lua
+++ dot_config/nvim/lua/plugins/ui/which-key.lua
@@ -6,17 +6,19 @@ return {
     local wk = require("which-key")
     wk.add({
       { "<leader>b",  group = "+buffer" },
-      { "<leader>c",  group = "+ai",                                                         mode = { "n", "v" } },
+      { "<leader>c",  group = "+ai", mode = { "n", "v" } },
       { "<leader>d",  group = "+debug" },
-      { "<leader>g",  group = "+git" },
-      { "<leader>gd", group = "+diff" },
       { "<leader>s",  group = "+search" },
       { "<leader>t",  group = "+test" },
       { "<leader>u",  group = "+utils" },
       { "<leader>uc", group = "+change_case_to" },
       { "<leader>um", group = "+markdown" },
       { "<leader>uw", group = "+window" },
-      { "<leader>w",  '<cmd>lua require("which-key").show({keys = "<c-w>", loop=true})<cr>', desc = "window" },
+      {
+        "<leader>w",
+        '<cmd>lua require("which-key").show({keys = "<c-w>", loop=true})<cr>',
+        desc = "window"
+      },
     })
   end,
 }
