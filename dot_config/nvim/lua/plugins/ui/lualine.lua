1450feb3213770ce2861902ee7113d9403b5d5d4
Luis Duque <luisfpetrucci@gmail.com>
2025-01-05 15:35:11 -0500

    feat(nvim): updated lualine to handle tabline

--- dot_config/nvim/lua/plugins/ui/lualine.lua
+++ dot_config/nvim/lua/plugins/ui/lualine.lua
@@ -2,13 +2,14 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+	        vim.opt.showtabline=0
 		require("lualine").setup({
 			options = {
 				theme = "gruvbox_light",
 			},
 			sections = {
 				lualine_b = { "branch" },
-				lualine_x = { "searchcount", "filetype" },
+				lualine_x = { "tabs", "searchcount", "filetype" },
 			},
 		})
 	end,
