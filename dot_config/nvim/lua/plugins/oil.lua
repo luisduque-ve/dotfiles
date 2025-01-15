28efbbcca027c124f9a95b344cb1b174ee5ff0ef
Luis Duque <luisfpetrucci@gmail.com>
2025-01-14 21:31:31 -0500

    feat(nvim): restored oil.nvim

---
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,26 @@
+return {
+	"stevearc/oil.nvim",
+	event = "VeryLazy",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local oil = require("oil")
+		oil.setup({
+			view_options = {
+				show_hidden = true,
+			},
+			keymaps = {
+				["<C-h>"] = false,
+				["<C-l>"] = false,
+				["q"] = "actions.close",
+				["<C-p>"] = {
+					callback = function()
+						oil.open_preview({ vertical = true, split = "botright" })
+					end,
+				},
+			},
+		})
+		-- not sure why this is not correctly handle in the
+		-- lazy's keys table
+		vim.keymap.set("n", "<leader>o", require("oil").open, { desc = "oil" })
+	end,
+}
