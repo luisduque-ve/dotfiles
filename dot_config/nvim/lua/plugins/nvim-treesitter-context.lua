b0142b0b820d744fef64807fffe7a43e6f328447
Luis Duque <luisfpetrucci@gmail.com>
2025-01-15 13:29:54 -0500

    fix(nvim): join treesitter plugins

--- dot_config/nvim/lua/plugins/nvim-treesitter-context.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-treesitter/nvim-treesitter-context",
-	event = "VeryLazy",
-	dependencies = "nvim-treesitter/nvim-treesitter-context",
-	opts = {
-		max_lines = 1,
-		trim_scope = "outer",
-	},
-}
