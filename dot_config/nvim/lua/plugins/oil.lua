985eacf469aa41ddc9056ab9159b365f7bd0d960
Luis Duque <luisfpetrucci@gmail.com>
2025-01-15 13:29:41 -0500

    fix(nvim): updated oil mapping

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -21,6 +21,6 @@ return {
 		})
 		-- not sure why this is not correctly handle in the
 		-- lazy's keys table
-		vim.keymap.set("n", "<leader>o", require("oil").open, { desc = "oil" })
+		vim.keymap.set("n", "<leader>d", require("oil").open, { desc = "oil" })
 	end,
 }
