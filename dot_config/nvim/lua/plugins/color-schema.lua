a73045d1605e93581c2e22bfae979d7fc9ba134c
Luis Duque <luisfpetrucci@gmail.com>
2025-02-06 16:40:21 -0500

    fix(nvim): deprecated light color schema

--- dot_config/nvim/lua/plugins/color-schema.lua
+++ dot_config/nvim/lua/plugins/color-schema.lua
@@ -1,17 +1,17 @@
 return {
-	"sainnhe/gruvbox-material",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd([[
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd([[
   if has('termguicolors')
     set termguicolors
   endif
-  set background=light
+  set background=dark
   let g:gruvbox_material_background = 'medium'
   let g:gruvbox_material_enable_bold = 1
   let g:gruvbox_material_better_performance = 1
   colorscheme gruvbox-material
 ]])
-	end,
+  end,
 }
