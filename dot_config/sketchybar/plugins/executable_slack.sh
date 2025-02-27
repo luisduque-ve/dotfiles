d026ec6c5b2e285aec0a6d88776236d57b4b134e
Luis Duque <luisfpetrucci@gmail.com>
2025-02-27 12:39:36 -0500

    fix(sketchybar): fixed slack notification

--- dot_config/sketchybar/plugins/executable_slack.sh
+++ dot_config/sketchybar/plugins/executable_slack.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-SLACK_COUNT=$(osascript -e '
-tell application "System Events"
-    set slackRunning to (exists process "Slack")
-    if slackRunning then
-        tell application "System Events" to tell process "Slack"
-            try
-                set unreadCount to (get value of attribute "AXTitle" of UI element 1 of list 1 of application process "Dock" whose role description is "Dock item" and name is "Slack")
-                if unreadCount is missing value then return 0
-                return unreadCount
-            on error
-                return 0
-            end try
-        end tell
-    else
-        return 0
-    end if
-end tell')
+SLACK_INFO=$(lsappinfo info -only StatusLabel `lsappinfo find LSDisplayName=Slack`)
+COUNT=${SLACK_INFO:25:1}
 
-if [ "$SLACK_COUNT" = "0" ]; then
-  ICON="􀌥"  # No unread messages (You can change this to another SF Symbol)
+if [ $COUNT = "\"" ]; then
+  DRAWING=off
 else
-  ICON="􁏊"  # Unread messages icon (Change as needed)
+  DRAWING=on
 fi
 
-sketchybar --set "$NAME" icon="$ICON" label="$SLACK_COUNT"
+sketchybar --set "$NAME" drawing=$DRAWING icon="$ICON" label="􀌥 $COUNT"
