2a97fc9faa3c86941841e9d11be4f67497a7b3c1
Luis Duque <luisfpetrucci@gmail.com>
2025-03-16 16:37:14 -0500

    fix(nvim): updated neogit config

--- dot_config/nvim/lua/plugins/neogit.lua
+++ dot_config/nvim/lua/plugins/neogit.lua
@@ -2,12 +2,25 @@ return {
   "NeogitOrg/neogit",
   cond = not vim.g.vscode,
   dependencies = {
-    "nvim-lua/plenary.nvim", -- required
+    "nvim-lua/plenary.nvim",  -- required
     "sindrets/diffview.nvim", -- optional - Diff integration
     -- Only one of these is needed.
-    "ibhagwan/fzf-lua",     -- optional
+    "ibhagwan/fzf-lua",       -- optional
   },
-  config = true,
+  config = {
+    -- disable_line_numbers = false,
+    disable_relative_line_numbers = false,
+    mappings = {
+      finder = {
+        ["<c-n>"] = false,
+        ["<c-p>"] = false,
+      },
+      status = {
+        ["<c-n>"] = false,
+        ["<c-p>"] = false,
+      },
+    },
+  },
   keys = {
     { "<leader>gg", "<CMD>Neogit kind=replace<CR>", desc = "neogit" },
   },
