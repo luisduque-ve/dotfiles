610abe845225d88e3515ea5df54a725f24e86bf2
Luis Duque <luisfpetrucci@gmail.com>
2025-03-16 21:57:29 -0500

    fix(nvim): updated neogit config, change config to opts

--- dot_config/nvim/lua/plugins/neogit.lua
+++ dot_config/nvim/lua/plugins/neogit.lua
@@ -1,27 +1,27 @@
 return {
-  "NeogitOrg/neogit",
-  cond = not vim.g.vscode,
-  dependencies = {
-    "nvim-lua/plenary.nvim",  -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
-    -- Only one of these is needed.
-    "ibhagwan/fzf-lua",       -- optional
-  },
-  config = {
-    -- disable_line_numbers = false,
-    disable_relative_line_numbers = false,
-    mappings = {
-      finder = {
-        ["<c-n>"] = false,
-        ["<c-p>"] = false,
-      },
-      status = {
-        ["<c-n>"] = false,
-        ["<c-p>"] = false,
-      },
-    },
-  },
-  keys = {
-    { "<leader>gg", "<CMD>Neogit kind=replace<CR>", desc = "neogit" },
-  },
+	"NeogitOrg/neogit",
+	cond = not vim.g.vscode,
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+		-- Only one of these is needed.
+		"ibhagwan/fzf-lua", -- optional
+	},
+	opts = {
+		-- disable_line_numbers = false,
+		disable_relative_line_numbers = false,
+		mappings = {
+			finder = {
+				["<c-n>"] = false,
+				["<c-p>"] = false,
+			},
+			status = {
+				["<c-n>"] = false,
+				["<c-p>"] = false,
+			},
+		},
+	},
+	keys = {
+		{ "<leader>gg", "<CMD>Neogit kind=replace<CR>", desc = "neogit" },
+	},
 }
