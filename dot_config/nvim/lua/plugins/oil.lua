30e3d279b288ea65644c2d506c7fc00c0354e261
Luis Duque <luisfpetrucci@gmail.com>
2025-03-16 22:05:51 -0500

    fix(nvim): updated oil mapping

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,29 +1,29 @@
 return {
-  "stevearc/oil.nvim",
-  cond = not vim.g.vscode,
-  event = "VeryLazy",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local oil = require("oil")
-    oil.setup({
-      view_options = {
-        show_hidden = true,
-      },
-      keymaps = {
-        ["<C-h>"] = false,
-        ["<C-l>"] = false,
-        ["<C-p>"] = false,
-        ["<C-n>"] = false,
-        ["q"] = "actions.close",
-        ["<C-e>"] = {
-          callback = function()
-            oil.open_preview({ vertical = true, split = "botright" })
-          end,
-        },
-      },
-    })
-    -- not sure why this is not correctly handle in the
-    -- lazy's keys table
-    vim.keymap.set("n", "<leader>d", require("oil").open, { desc = "oil" })
-  end,
+	"stevearc/oil.nvim",
+	cond = not vim.g.vscode,
+	event = "VeryLazy",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local oil = require("oil")
+		oil.setup({
+			view_options = {
+				show_hidden = true,
+			},
+			keymaps = {
+				["<C-h>"] = false,
+				["<C-l>"] = false,
+				["<C-p>"] = false,
+				["<C-n>"] = false,
+				["q"] = "actions.close",
+				["<C-e>"] = {
+					callback = function()
+						oil.open_preview({ vertical = true, split = "botright" })
+					end,
+				},
+			},
+		})
+		-- not sure why this is not correctly handle in the
+		-- lazy's keys table
+		vim.keymap.set("n", "<leader>.", require("oil").open, { desc = "oil" })
+	end,
 }
