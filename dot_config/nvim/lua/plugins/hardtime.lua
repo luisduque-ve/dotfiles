ce70a8557a17031dd0e186f4f6c4948828f95251
Luis Duque <luisfpetrucci@gmail.com>
2025-04-28 16:33:53 -0500

    fix(nvim): updated hardmode config

--- dot_config/nvim/lua/plugins/hardtime.lua
+++ dot_config/nvim/lua/plugins/hardtime.lua
@@ -2,6 +2,10 @@ return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 	opts = {
+		disabled_keys = {
+			["<Left>"] = { "n", "x" },
+			["<Right>"] = { "n", "x" },
+		},
 		allow_different_keys = true,
 	},
 	command = "Hardtime",
