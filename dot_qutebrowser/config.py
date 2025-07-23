70340ad37011cefc41419e885fb4b9c34ed47354
Luis Duque <luisfpetrucci@gmail.com>
2025-07-23 12:52:42 -0500

    fix(qutebrower): fixed google meet issues

--- dot_qutebrowser/config.py
+++ dot_qutebrowser/config.py
@@ -5,3 +5,13 @@ c.tabs.width = 150
 c.completion.shrink = True
 config.bind("<Alt-x>", "cmd-set-text :")
 config.bind("<Alt-x>", "cmd-set-text :", mode="insert")
+
+
+## google meet settings
+with config.pattern("*://meet.google.com/*") as p:
+    p.content.media.audio_video_capture = True
+    p.content.notifications.enabled = True
+
+## This is a workaround to fix a google meet permissions issue
+## see here: https://github.com/qutebrowser/qutebrowser/issues/8539#issuecomment-2784470927
+c.qt.args = ["disable-features=PermissionElement"]
