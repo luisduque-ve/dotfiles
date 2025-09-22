1eb8a77a8a8862acdaf084580d25b2ae49f67f6c
Luis Duque <luisfpetrucci@gmail.com>
2025-09-22 12:17:21 -0500

    feat(fish): updated generate conflict function

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -105,11 +105,11 @@ function conflict_generator
     # Create a new branch
     git branch conflict_branch
     # Modify the file in the master branch
-    echo "Content added in master branch" >> dummy.txt
+    echo "Content added in master branch ************************************************************************************************" >> dummy.txt
     git commit -am "Modify file in master branch"
     # Switch to the conflict branch and modify the file
     git checkout conflict_branch
-    echo "Content added in conflict branch" >> dummy.txt
+    echo "Content added in conflict branch ************************************************************************************************" >> dummy.txt
     git commit -am "Modify file in conflict branch"
     echo "Setup complete. You can now try to merge 'conflict_branch' into 'master' to create a conflict."
     cd ..
